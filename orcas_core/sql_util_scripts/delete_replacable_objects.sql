9cd074397914f568c8a8e028ba9f169cde498232
Olaf Jessensky <Olaf.Jessensky@opitz-consulting.com>
2014-11-28 13:30:30 +0100

    Fix für Jenkins-Build mit Extensions

--- orcas_core/sql_util_scripts/delete_replacable_objects.sql
+++ orcas_core/sql_util_scripts/delete_replacable_objects.sql
@@ -6,31 +6,26 @@ begin
       from user_objects 
      where object_type = 'PACKAGE' 
        and &1
-    union
-    select 'drop library ' || OBJECT_NAME  text
-      from user_objects 
-     where object_type = 'LIBRARY' 
-       and &2
     union 	
     select 'drop trigger ' || OBJECT_NAME  text
       from user_objects 
      where object_type = 'TRIGGER'
-       and &3
+       and &2
    union
     select 'drop view ' || OBJECT_NAME  text
       from user_objects 
      where object_type = 'VIEW'
-       and &4
+       and &3
    union
     select 'drop function ' || OBJECT_NAME text
       from user_objects
     where object_type = 'FUNCTION'
-       and &5
+       and &4
     union
     select 'drop procedure ' || OBJECT_NAME text
       from user_objects
     where object_type = 'PROCEDURE'
-       and &6
+       and &5
     )
      loop
     execute immediate cur_sql.text;
