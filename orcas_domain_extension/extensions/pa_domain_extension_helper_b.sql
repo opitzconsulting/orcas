073e1d40f1c69d25c0ca6f4268889336078fb09b
Frank Sanders <frank.sanders@opitz-consulting.de>
2014-12-12 12:27:16 +0100

    domanextension remove name rule

--- orcas_domain_extension/extensions/pa_domain_extension_helper_b.sql
+++ orcas_domain_extension/extensions/pa_domain_extension_helper_b.sql
@@ -61,17 +61,45 @@ create or replace package body pa_domain_extension_helper is
   function get_generated_name( p_gennamerule_list in ct_syex_gennamerule_list, p_column_name in varchar2, p_column_domain_name in varchar2, p_table_name in varchar2, p_alias in varchar2 ) return varchar2
   is
     v_return varchar2(100) := '';
+    v_remove_next number := 0;
+    v_remove_token varchar2(100); 
+    
+    procedure handle_next_token( p_token in varchar2 )
+    is
+    begin
+      if( p_token is not null )
+      then
+        if( v_remove_next = 1 )        
+        then
+          v_remove_token :=  v_remove_token || p_token;            
+          v_remove_next := 0; 
+        else
+          v_return := v_return || p_token;            
+          
+          if( v_remove_token is not null )
+          then
+            if( instr( v_return, v_remove_token ) < 1 )
+            then
+              raise_application_error( -20000, 'token not found ' || v_return || ' ' || v_remove_token );
+            end if;
+            
+            v_return := replace( v_return, v_remove_token, '' );
+            v_remove_token := '';
+          end if;
+        end if;
+      end if;
+    end;
   begin
     for i in 1..p_gennamerule_list.count
     loop
-      v_return := v_return || p_gennamerule_list(i).i_constant_name;
+      handle_next_token( p_gennamerule_list(i).i_constant_name );
       if( ot_syex_gennamerulepart.is_equal( p_gennamerule_list(i).i_constant_part, ot_syex_gennamerulepart.c_column_name ) = 1 )
       then
         if( p_column_name is null )
         then
           raise_application_error( -20000, 'p_column_name invalid' || p_alias || p_table_name );
         end if;
-        v_return := v_return || p_column_name;
+        handle_next_token( p_column_name );
       end if;
       if( ot_syex_gennamerulepart.is_equal( p_gennamerule_list(i).i_constant_part, ot_syex_gennamerulepart.c_column_domain_name ) = 1 )
       then
@@ -79,7 +107,7 @@ create or replace package body pa_domain_extension_helper is
         then
           raise_application_error( -20000, 'p_column_domain_name invalid' || p_column_name || p_table_name );
         end if;      
-        v_return := v_return || p_column_domain_name;
+        handle_next_token( p_column_domain_name );
       end if;
       if( ot_syex_gennamerulepart.is_equal( p_gennamerule_list(i).i_constant_part, ot_syex_gennamerulepart.c_table_name ) = 1 )
       then
@@ -87,11 +115,15 @@ create or replace package body pa_domain_extension_helper is
         then
           raise_application_error( -20000, 'p_table_name invalid' || p_column_name || p_alias );
         end if;      
-        v_return := v_return || p_table_name;
+        handle_next_token( p_table_name );
       end if;
       if( ot_syex_gennamerulepart.is_equal( p_gennamerule_list(i).i_constant_part, ot_syex_gennamerulepart.c_alias_name ) = 1 )
       then
-        v_return := v_return || p_alias;
+        handle_next_token( p_alias );
+      end if;
+      if( ot_syex_gennamerulepart.is_equal( p_gennamerule_list(i).i_constant_part, ot_syex_gennamerulepart.c_remove_next ) = 1 )
+      then
+        v_remove_next := 1;
       end if;
     end loop;
 
