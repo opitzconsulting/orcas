8c1946bb2f0c3e823d90736ba90912fcb56195d9
Frank Sanders <frank.sanders@opitz-consulting.de>
2015-03-13 16:09:00 +0100

    authid current user fuer extensions gesetzt

--- orcas_core/build_source/orcas_db_objects/packages/h_pa_orcas_extensions.sql
+++ orcas_core/build_source/orcas_db_objects/packages/h_pa_orcas_extensions.sql
@@ -1,4 +1,4 @@
-create or replace package pa_orcas_extensions is
+create or replace package pa_orcas_extensions authid current_user is
   function call_extensions( p_input in ot_syex_model ) return ot_syex_model;
   function call_reverse_extensions( p_input in ot_syex_model ) return ot_syex_model;
 end;
