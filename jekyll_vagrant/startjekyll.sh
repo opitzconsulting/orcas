5d0c3a5ab5a4aabcdce3e7e7d73c2c4f889d8e7b
Frank Sanders <frank.sanders@opitz-consulting.com>
2017-02-24 10:36:14 +0100

    doc

--- jekyll_vagrant/startjekyll.sh
+++ jekyll_vagrant/startjekyll.sh
@@ -1,4 +1,4 @@
 cd /orcas/orcas
 
-jekyll s --watch --force_polling --baseurl --incremental '' --host=0.0.0.0 -d /tmp/orcas_jekyll
+jekyll s --watch --force_polling --baseurl '' --incremental --host=0.0.0.0 -d /tmp/orcas_jekyll
 
