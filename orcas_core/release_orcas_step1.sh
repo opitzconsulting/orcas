88f7e2f74495f70ebba18cfc0de245f420b82f28
Frank Sanders <frank.sanders@opitz-consulting.com>
2019-01-16 10:44:23 +0100

    new version

---
+++ orcas_core/release_orcas_step1.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+new_orcas_version=5.3.1
+
+cd ..
+
+sed -zi -e "s/\(\(orcas.[^\n]*[\n][^\n]*\)\|\(project.version[^\n]*\)\)\([0-9]\+\.[0-9]\+\.[0-9]\+\)\(-SNAPSHOT\)\?/\1$new_orcas_version/g" orcas_core/build_source/build.gradle orcas_maven_plugin/pom.xml orcas_integrationstest/build.gradle orcas_core/upload_maven_central/build.gradle
+
+cd orcas_core/build_source
+
+./gradlew clean
+./gradlew publishToMavenLocal -Pextensionname_internal=domainextension -Porcas_extension_folder=../../../orcas_domain_extension_java/extensions -Porcas_extension_extract_file=../../../orcas_domain_extension_java/xslt_extract/orcas_domain_extract.xsl
+./gradlew publishToMavenLocal 
+
+cd ../../orcas_maven_plugin
+
+mvn clean install
+
+cd ../orcas_core/upload_maven_central
+
+./gradlew clean publish
+
+
