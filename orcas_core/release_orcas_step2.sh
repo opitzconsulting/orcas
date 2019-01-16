680a6cf8b0a2bc6d7ea8bd933a90725a1c1f4f90
Frank Sanders <frank.sanders@opitz-consulting.com>
2019-01-16 11:05:59 +0100

    new snapshot

---
+++ orcas_core/release_orcas_step2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+new_orcas_snapshot_version=5.3.2-SNAPSHOT
+
+cd ..
+
+sed -zi -e "s/\(\(orcas.[^\n]*[\n][^\n]*\)\|\(project.version[^\n]*\)\)\([0-9]\+\.[0-9]\+\.[0-9]\+\)\(-SNAPSHOT\)\?/\1$new_orcas_snapshot_version/g" orcas_core/build_source/build.gradle orcas_maven_plugin/pom.xml 
+
