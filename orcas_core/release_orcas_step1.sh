b15ba6cc89a5638cb2b8fc1e8de62a6c90ae5795
Frank Sanders <frank.sanders@opitz-consulting.com>
2023-03-13 12:30:36 +0100

    release

--- orcas_core/release_orcas_step1.sh
+++ orcas_core/release_orcas_step1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-new_orcas_version=8.0.1
+new_orcas_version=8.0.2
 
 cd ..
 
--- orcas_core/release_orcas_step2.sh
+++ orcas_core/release_orcas_step2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-new_orcas_snapshot_version=8.0.2-SNAPSHOT
+new_orcas_snapshot_version=8.0.3-SNAPSHOT
 
 cd ..
 
